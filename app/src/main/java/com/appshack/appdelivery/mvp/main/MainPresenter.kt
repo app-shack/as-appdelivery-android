b0b68fc7420a2f97baeee6edc2a8c42b4a58677e
Joel Brostrom <joel.brostrom@appshack.se>
2018-08-03 19:55:48 +0200

    Compare current version with server
    Using the data obtained as a data class I compare the
    version one sub-version at a time.
    I have to chain the comparisons since each value have a
    higher significance than the following. i.e I cant just treat it
    as an Int.

--- app/src/main/java/com/appshack/appdelivery/mvp/main/MainPresenter.kt
+++ app/src/main/java/com/appshack/appdelivery/mvp/main/MainPresenter.kt
@@ -1,11 +1,10 @@
 package com.appshack.appdelivery.mvp.main
 
+import com.appshack.appdelivery.network.api.models.VersionDataModel
+import com.appshack.appdelivery.network.api.parsers.ResponseParser
 import com.appshack.appdelivery.network.api.requests.APIRequest
 import com.appshack.appdelivery.network.api.requests.UpdateRequest
 import com.appshack.appdelivery.network.dispatchers.Dispatcher
-import com.appshack.appdelivery.network.api.parsers.ResponseParser
-import com.appshack.appdelivery.network.api.models.UpdateResponseModel
-import java.io.IOException
 
 
 /**
@@ -13,6 +12,7 @@ import java.io.IOException
  * Developed by App Shack
  */
 class MainPresenter(val activity: MainActivityPresenterRelations.Activity) : MainActivityPresenterRelations.Presenter {
+
     override fun checkForUpdates() {
         activity.setTextViewText("Fetching...")
         val apiRequest: APIRequest = UpdateRequest()
@@ -21,25 +21,57 @@ class MainPresenter(val activity: MainActivityPresenterRelations.Activity) : Mai
 
     }
 
+    fun evaluateVersionStatus(versionDataModel: VersionDataModel) {
+        with(versionDataModel) {
+            val currentVersion = currentVersion?.split(".")?.map { it.toInt() } ?: listOf(0, 0, 0)
+            val minimumVersion = requiredVersion?.split(".")?.map { it.toInt() } ?: listOf(0, 0, 0)
+            val lastVersion = latestVersion?.split(".")?.map { it.toInt() } ?: listOf(0, 0, 0)
+
+            isUpdateRequired = isVersionGraterThen(currentVersion, minimumVersion)
+            isUpdateAvailable = isVersionGraterThen(currentVersion, lastVersion)
+
+        }
+
+    }
+
+    private fun isVersionGraterThen(leftVersion: List<Int>, rightVersion: List<Int>): Boolean {
+        var isLeftGreater: Boolean? = null
+        for ((index, left) in leftVersion.withIndex()) {
+            when {
+                left > rightVersion[index] && isLeftGreater == null -> isLeftGreater = false
+                left < rightVersion[index] && isLeftGreater == null -> isLeftGreater = true
+            }
+        }
+        return isLeftGreater ?: false
+    }
+
     private val onResultCallback: ResultCallback = object : ResultCallback {
 
-        override fun onComplete(result: UpdateResponseModel?) {
-            with((result as UpdateResponseModel).data!!) {
-                android.util.Log.i("@dev Response", identifier)
-                android.util.Log.i("@dev Response", latestVersion)
-                android.util.Log.i("@dev Response", requiredVersion)
-                activity.setTextViewText(identifier + latestVersion + requiredVersion)
+        override fun onComplete(versionDataModel: VersionDataModel?) {
+            versionDataModel?.let {
+                android.util.Log.i("@dev Response", it.identifier)
+                android.util.Log.i("@dev Response", it.latestVersion)
+                android.util.Log.i("@dev Response", it.requiredVersion)
+
+                it.currentVersion = activity.packageInformation.versionName
+                evaluateVersionStatus(it)
+                activity.setTextViewText("name: ${it.identifier}\n" +
+                        "current version: ${it.currentVersion}\n" +
+                        "min version: ${it.requiredVersion} \n" +
+                        "latest version: ${it.latestVersion}\n\n" +
+                        "update required: ${it.isUpdateRequired}\n" +
+                        "update available: ${it.isUpdateAvailable}")
             }
         }
 
-        override fun onFailure(error: IOException?) {
-            activity.setTextViewText(error?.toString() ?: "Unknown error")
+        override fun onFailure(error: String?) {
+            activity.setTextViewText(error ?: "Unknown error")
         }
 
     }
 }
 
 interface ResultCallback {
-    fun onComplete(result: UpdateResponseModel?)
-    fun onFailure(error: IOException?)
+    fun onComplete(result: VersionDataModel?)
+    fun onFailure(error: String?)
 }
