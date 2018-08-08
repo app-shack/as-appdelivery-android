1c35f1b8950764fa8df1ece2530857c22911d399
Joel Brostrom <joel.brostrom@appshack.se>
2018-08-08 15:56:39 +0200

    Add enum for all possible result statuses
    Describes what version state the app is in in relation to the
    latest server version, and what action should be taken.

---
+++ app/src/main/java/com/appshack/appdelivery/entity/VersionResultCode.kt
@@ -0,0 +1,13 @@
+package com.appshack.appdelivery.entity
+
+
+/**
+ * Created by joelbrostrom on 2018-08-07
+ * Developed by App Shack
+ */
+enum class VersionResultCode {
+    UPDATE_REQUIRED(),
+    UPDATE_AVAILABLE(),
+    UP_TO_DATE(),
+    ERROR()
+}
